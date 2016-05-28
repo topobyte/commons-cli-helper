88d14dd4032dc48fbca728115139012910b67731
Sebastian Kürten <sebastian@topobyte.de>
2016-05-28 18:42:23 +0200

    Added a test for basic usage of ArgumentHelper

---
+++ src/test/java/de/topobyte/utilities/apache/commons/cli/TestBasic.java
@@ -0,0 +1,37 @@
+package de.topobyte.utilities.apache.commons.cli;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.junit.Test;
+
+import de.topobyte.utilities.apache.commons.cli.parsing.ArgumentHelper;
+import de.topobyte.utilities.apache.commons.cli.parsing.StringOption;
+
+public class TestBasic
+{
+
+	@Test
+	public void main() throws ParseException
+	{
+		Options options = new Options();
+		OptionHelper.add(options, "foo", true, true, "an option");
+		OptionHelper.add(options, "bar", true, true, "another option");
+
+		String[] arguments = new String[] { "-foo", "asdf", "-bar", "test" };
+		CommandLine line = new DefaultParser().parse(options, arguments);
+
+		StringOption foo = ArgumentHelper.getString(line, "foo");
+		StringOption bar = ArgumentHelper.getString(line, "bar");
+
+		assertTrue(foo.hasValue());
+		assertTrue(bar.hasValue());
+		assertEquals("asdf", foo.getValue());
+		assertEquals("test", bar.getValue());
+	}
+
+}

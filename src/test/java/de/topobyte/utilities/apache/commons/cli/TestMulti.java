65e1ed1c0081ce39cb5471a6272982ca75138003
Sebastian Kürten <sebastian@topobyte.de>
2016-05-28 18:53:23 +0200

    Some tests for methods for multiple arguments

---
+++ src/test/java/de/topobyte/utilities/apache/commons/cli/TestMulti.java
@@ -0,0 +1,98 @@
+package de.topobyte.utilities.apache.commons.cli;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.junit.Test;
+
+import de.topobyte.utilities.apache.commons.cli.parsing.ArgumentHelper;
+import de.topobyte.utilities.apache.commons.cli.parsing.ArgumentParseException;
+import de.topobyte.utilities.apache.commons.cli.parsing.BooleanOption;
+import de.topobyte.utilities.apache.commons.cli.parsing.IntegerOption;
+import de.topobyte.utilities.apache.commons.cli.parsing.StringOption;
+
+public class TestMulti
+{
+
+	@Test
+	public void testString() throws ParseException
+	{
+		Options options = new Options();
+		OptionHelper.add(options, "foo", true, true, "an option");
+		OptionHelper.add(options, "bar", true, true, "another option");
+
+		String[] arguments = new String[] { "-foo", "asdf", "-bar", "test",
+				"-bar", "more" };
+		CommandLine line = new DefaultParser().parse(options, arguments);
+
+		StringOption foo = ArgumentHelper.getString(line, "foo");
+		StringOption bar = ArgumentHelper.getString(line, "bar");
+
+		assertTrue(foo.hasValue());
+		assertTrue(bar.hasValue());
+		assertEquals("asdf", foo.getValue());
+		assertEquals("test", bar.getValue());
+
+		List<StringOption> bars = ArgumentHelper.getStrings(line, "bar");
+		assertEquals(2, bars.size());
+		assertEquals("test", bars.get(0).getValue());
+		assertEquals("more", bars.get(1).getValue());
+	}
+
+	@Test
+	public void testBoolean() throws ParseException, ArgumentParseException
+	{
+		Options options = new Options();
+		OptionHelper.add(options, "foo", true, true, "an option");
+		OptionHelper.add(options, "bar", true, true, "another option");
+
+		String[] arguments = new String[] { "-foo", "true", "-bar", "true",
+				"-bar", "false" };
+		CommandLine line = new DefaultParser().parse(options, arguments);
+
+		BooleanOption foo = ArgumentHelper.getBoolean(line, "foo");
+		BooleanOption bar = ArgumentHelper.getBoolean(line, "bar");
+
+		assertTrue(foo.hasValue());
+		assertTrue(bar.hasValue());
+		assertEquals(true, foo.getValue());
+		assertEquals(true, bar.getValue());
+
+		List<BooleanOption> bars = ArgumentHelper.getBooleans(line, "bar");
+		assertEquals(2, bars.size());
+		assertEquals(true, bars.get(0).getValue());
+		assertEquals(false, bars.get(1).getValue());
+	}
+
+	@Test
+	public void testInteger() throws ParseException, ArgumentParseException
+	{
+		Options options = new Options();
+		OptionHelper.add(options, "foo", true, true, "an option");
+		OptionHelper.add(options, "bar", true, true, "another option");
+
+		String[] arguments = new String[] { "-foo", "10", "-bar", "20", "-bar",
+				"30" };
+		CommandLine line = new DefaultParser().parse(options, arguments);
+
+		IntegerOption foo = ArgumentHelper.getInteger(line, "foo");
+		IntegerOption bar = ArgumentHelper.getInteger(line, "bar");
+
+		assertTrue(foo.hasValue());
+		assertTrue(bar.hasValue());
+		assertEquals(10, foo.getValue());
+		assertEquals(20, bar.getValue());
+
+		List<IntegerOption> bars = ArgumentHelper.getIntegers(line, "bar");
+		assertEquals(2, bars.size());
+		assertEquals(20, bars.get(0).getValue());
+		assertEquals(30, bars.get(1).getValue());
+	}
+
+}

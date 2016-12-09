8194c396290ed9e2725b997d2402669b5e7c1f66
Sebastian Kürten <sebastian@topobyte.de>
2016-12-09 20:18:50 +0100

    Add some missing license headers

--- src/test/java/de/topobyte/utilities/apache/commons/cli/TestMulti.java
+++ src/test/java/de/topobyte/utilities/apache/commons/cli/TestMulti.java
@@ -1,3 +1,20 @@
+// Copyright 2016 Sebastian Kuerten
+//
+// This file is part of commons-cli-helper.
+//
+// commons-cli-helper is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// commons-cli-helper is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with commons-cli-helper. If not, see <http://www.gnu.org/licenses/>.
+
 package de.topobyte.utilities.apache.commons.cli;
 
 import static org.junit.Assert.assertEquals;
